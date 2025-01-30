29fbd5ed9bfbe48a28764543e90148ceb9228264
Zheng Lin Lei <zheng9112003@gmail.com>
2025-01-30 15:28:08 +0100

    init: Project

---
+++ include/lib_arisr_interface.h
@@ -0,0 +1,42 @@
+/**
+ * @attention
+
+    Copyright (C) 2025  - ARIS Alliance
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ **********************************************************************************
+ * @file lib_arisr_interface.h
+ * @brief This file contains the interface of the ARISr protocol.
+ * @date 2025-01-30
+ * @authors ARIS Alliance
+*/
+
+#ifndef LIB_ARISR_INTERFACE_H
+#define LIB_ARISR_INTERFACE_H
+
+
+/*
+
+    ARIS Radio protocol interface
+
+    Protocol interface: ../docs/ARISRadioProtocol.pdf
+*/
+
+
+#endif
+
+
+/* COPYRIGHT ARIS Alliance */
---
+++ source/lib_arisr.c
@@ -0,0 +1,31 @@
+/**
+ * @attention
+
+    Copyright (C) 2025  - ARIS Alliance
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ **********************************************************************************
+ * @file lib_arisr.c
+ * @brief This file contains the implementation of the ARISr library.
+ * @date 2025-01-30
+ * @authors ARIS Alliance
+*/
+
+#include "lib_arisr.h"
+
+
+
+/* COPYRIGHT ARIS Alliance */
